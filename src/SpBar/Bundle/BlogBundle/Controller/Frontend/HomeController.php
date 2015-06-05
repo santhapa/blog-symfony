431fc1203af99c25e2c46e367f0e1736861ec78c
santhapa <san.loggingin@gmail.com>
2015-06-05 12:49:10 +0545

    author posts url

--- src/SpBar/Bundle/BlogBundle/Controller/Frontend/HomeController.php
+++ src/SpBar/Bundle/BlogBundle/Controller/Frontend/HomeController.php
@@ -12,38 +12,19 @@ class HomeController extends Controller
 {
 	public function indexAction(Request $request)
 	{
-		$postManager = $this->get('spbar.blog_post_manager');
-		
-		if ($request->query->get('author')) {
-			$user = $this->get('fos_user.user_manager')->findUserByUsername($request->query->get('author'));
-			if (!$user) {
-				throw $this->createNotFoundException();
-			}
-			$author = trim($user->getName()) ? $user->getName() : $user->getUsername();
-			$dbPosts = $postManager->getPostsByAuthor($user);
-			$title = "Posts By: {$author}";
-		}else
-		{
-			$dbPosts = $postManager->getPosts();
-			$title = "Blog";
-		}
-
-		$paginator  = $this->get('knp_paginator');
-	    $posts = $paginator->paginate(
-	        $dbPosts,
-	        $request->query->get('page', 1)/*page number*/,
-	        $this->get('spbar.blog_config_manager')->get('post_per_page')/*limit per page*/
-	    );
+		$posts = $this->getPosts($request);
 
 	    $breadcrumbs = $this->container->get("white_october_breadcrumbs");
 	    $breadcrumbs->addRouteItem("Home", "sp_blog_front_home_index");
-	    $breadcrumbs->addRouteItem("Blog", "sp_blog_front_home_index");
+	    // $breadcrumbs->addRouteItem("Blog", "sp_blog_front_home_index");
 
 		return $this->render("SpBarBlogBundle::Frontend/index.html.twig", array(
 			'posts'=>$posts,
-			'page_title'=> $title,
+			'page_title'=> 'Blog',
 		));
 	}
+
+	
 
 	public function singleAction(Request $request, $slug)
 	{
@@ -63,6 +44,47 @@ class HomeController extends Controller
 			'form' => $form->createView(),
 			'page_title'=> $post->getTitle(),
 		));
+	}
+
+	public function authorAction(Request $request, $author)
+	{
+		$user = $this->get('fos_user.user_manager')->findUserByUsername($author);
+		if (!$user) {
+			throw $this->createNotFoundException();
+		}
+		$authorName = trim($user->getName()) ? $user->getName() : $user->getUsername();
+
+		$posts = $this->getPosts($request, array('author'=>$user));
+
+	    $breadcrumbs = $this->container->get("white_october_breadcrumbs");
+	    $breadcrumbs->addRouteItem("Home", "sp_blog_front_home_index");
+	    $breadcrumbs->addItem("Author");
+
+		return $this->render("SpBarBlogBundle::Frontend/index.html.twig", array(
+			'posts'=>$posts,
+			'page_title'=> $authorName,
+		));
+	}
+
+	public function getPosts($request, $array= null)
+	{
+		$postManager = $this->get('spbar.blog_post_manager');
+		if(!$array)
+			$dbPosts = $postManager->getPosts();
+		if($array && array_key_exists('author', $array))
+		{
+			$dbPosts = $postManager->getPostsByAuthor($array['author']);
+		}
+		
+
+		$paginator  = $this->get('knp_paginator');
+	    $posts = $paginator->paginate(
+	        $dbPosts,
+	        $request->query->get('page', 1)/*page number*/,
+	        $this->get('spbar.blog_config_manager')->get('post_per_page')/*limit per page*/
+	    );
+
+	    return $posts;
 	}
 
 	// public function authorPostAction(Request $request, $user)
